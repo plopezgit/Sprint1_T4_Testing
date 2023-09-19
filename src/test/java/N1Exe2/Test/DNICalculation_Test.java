7ea855cb337a3298ac23345d98d25a5858e30a20
pedrolopez <pedrolopez@10.201.1.213>
2023-09-19 21:04:16 +0200

    declares @ValueSource annotation based on task suggestion

--- src/test/java/N1Exe2/Test/DNICalculation_Test.java
+++ src/test/java/N1Exe2/Test/DNICalculation_Test.java
@@ -6,12 +6,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import N1Exe1.MonthList;
 
 public class DNICalculation_Test {
 	
 	@ParameterizedTest
+	@ValueSource ( ints = {30359622, 30259622, 20458723, 30145387, 40656789, 24356743, 54378923, 30347893,
+								36234567, 20247645})
 	public void checkGetDNILetter () {		
 		
 	}
