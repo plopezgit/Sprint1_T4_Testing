cf93d87d320c5a3608a052d3c37d9f4e6426f083
pedrolopez <pedrolopez@localhost>
2023-09-20 11:06:16 +0200

    adds TestMethodOrder annotation on MonthList_Test Class; creates the OutOfBound and OutOfBound_Test Classes; it uses RepeteatedTest annotation; demostrations and test execution tests passed

---
+++ src/main/java/N1Exe3/OutOfBound.java
@@ -0,0 +1,18 @@
+package N1Exe3;
+
+import java.util.Random;
+
+public class OutOfBound {
+	
+	Random random;
+	
+	public OutOfBound() {
+		random = new Random();
+	}
+
+	public void printRandomObject () throws ArrayIndexOutOfBoundsException {
+		String[] arrayObj = { "obj1", "obj2", "obj3" };
+		System.out.println(arrayObj[random.nextInt(arrayObj.length + arrayObj.length)]);	
+	}
+
+}
--- src/test/java/N1Exe1/Test/MonthList_Test.java
+++ src/test/java/N1Exe1/Test/MonthList_Test.java
@@ -4,10 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import N1Exe1.MonthList;
 
+@TestMethodOrder(OrderAnnotation.class)
 public class MonthList_Test {
 	
 	private MonthList monthListTest;
@@ -21,8 +25,10 @@ public class MonthList_Test {
 		expectedSpecificIndexValueToTest = "Aug";
 	}
 	
+	
 	@DisplayName("Dado que una Lista de meses es rellenada, validar que la lista no esta vacia")
 	@Test
+	@Order(1)
 	public void checkMonthListNotNull () {
 		boolean monthListIsEmpty = monthListTest.addMonths().isEmpty();
 		
@@ -31,6 +37,7 @@ public class MonthList_Test {
 	
 	@DisplayName("Dado que una Lista de meses es rellenada, validar que el size de la lista sea el esperado")
 	@Test
+	@Order(2)
 	public void checkMonthListSize () {
 		int monthListTestSize = monthListTest.addMonths().size();
 		assertEquals(expectedSizeValueToTest, monthListTestSize);
@@ -38,6 +45,7 @@ public class MonthList_Test {
 	
 	@DisplayName("Dado que una Lista de meses es rellenada, validar el valor de un indice especifico")
 	@Test
+	@Order(3)
 	public void checkMonthListAugust () {
 		String monthListSpecificMonth = monthListTest.addMonths().get(7);
 		assertEquals(expectedSpecificIndexValueToTest, monthListSpecificMonth);
